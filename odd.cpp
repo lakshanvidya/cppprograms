155174745554fc584c5a7f5ba173e540070b133c
lakshanvidya <srividyak1810@gmail.com>
2017-02-02 15:04:01 +0530

    Add files via upload

---
+++ odd.cpp
@@ -0,0 +1,13 @@
+    #include <iostream>
+    using namespace std;
+     
+    int main()
+    {
+    int a;
+    cout<<"enter any non zero number";
+    cin>>a;
+    (a>0)?cout<<"number is positive":cout<<"number is negative";
+     
+    // your code goes here
+    return 0;
+    }
